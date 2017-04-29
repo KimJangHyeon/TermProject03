cb9cab5f81a031b2298b167b5c2a3bb0fda72542
KimJangHyeon <rlawkdgus75@gmail.com>
2017-04-29 12:39:03 +0900

    second

---
+++ app/src/main/java/com/example/rlawk/tremproject3/CallList.java
@@ -0,0 +1,13 @@
+package com.example.rlawk.tremproject3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CallList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.call_list);
+    }
+}
